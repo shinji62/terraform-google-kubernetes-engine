6ac87a8519a7d9c231d96d92655aa076b328fb4b
Etourneau Gwenn <gwenn.etourneau@hashicorp.com>
2024-12-23 16:36:34 +0900

    Disable dns

--- masq.tf
+++ masq.tf
@@ -19,29 +19,29 @@
 /******************************************
   Create ip-masq-agent confimap
  *****************************************/
-resource "kubernetes_config_map" "ip_masq_agent" {
-  count = var.configure_ip_masq ? 1 : 0
+# resource "kubernetes_config_map" "ip_masq_agent" {
+#   count = var.configure_ip_masq ? 1 : 0
 
-  metadata {
-    name      = "ip-masq-agent"
-    namespace = "kube-system"
+#   metadata {
+#     name      = "ip-masq-agent"
+#     namespace = "kube-system"
 
-    labels = {
-      maintained_by = "terraform"
-    }
-  }
+#     labels = {
+#       maintained_by = "terraform"
+#     }
+#   }
 
-  data = {
-    config = <<EOF
-nonMasqueradeCIDRs:
-  - ${join("\n  - ", var.non_masquerade_cidrs)}
-resyncInterval: ${var.ip_masq_resync_interval}
-masqLinkLocal: ${var.ip_masq_link_local}
-EOF
-  }
+#   data = {
+#     config = <<EOF
+# nonMasqueradeCIDRs:
+#   - ${join("\n  - ", var.non_masquerade_cidrs)}
+# resyncInterval: ${var.ip_masq_resync_interval}
+# masqLinkLocal: ${var.ip_masq_link_local}
+# EOF
+#   }
 
-  depends_on = [
-    google_container_cluster.primary,
-    google_container_node_pool.pools,
-  ]
-}
+#   depends_on = [
+#     google_container_cluster.primary,
+#     google_container_node_pool.pools,
+#   ]
+# }
